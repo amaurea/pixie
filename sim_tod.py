d6720ff6dc82a463bac06c0628280efee79c0e6b
Sigurd Næss <sigurdkn@gmail.com>
2016-06-04 14:03:45 +0100

    Forgot to add sim_tod.py

---
+++ sim_tod.py
@@ -0,0 +1,23 @@
+import numpy as np, argparse, pixie, imp
+from enlib import mpi, utils, log
+parser = argparse.ArgumentParser()
+parser.add_argument("orbits")
+parser.add_argument("odir")
+parser.add_argument("-C", "--config", type=str, default=None)
+args = parser.parse_args()
+
+orbits = pixie.parse_ints(args.orbits)
+comm   = mpi.COMM_WORLD
+L      = log.init(rank=comm.rank, shared=False, level='DEBUG')
+
+# Build our actual simulator. This is shared between tods
+config = pixie.load_config(args.config)
+sim    = pixie.PixieSim(config)
+utils.mkdir(args.odir)
+
+for ind in range(comm.rank, len(orbits), comm.size):
+	orbit = orbits[ind]
+	L.info("orbit %3d" % orbit)
+	tod   = sim.gen_tod_orbit(orbit)
+	pixie.write_tod(args.odir + "/tod%03d.hdf" % orbit, tod)
+	del tod
