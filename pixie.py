Fixed polarization amplitude relative to T in tod2ring. Overall units must still be fixed, though. Tried reversing angle correction in PointingGenerator

pixie.py
@@ -394,8 +394,8 @@ class PointingGenerator:
 		angpos[0] = utils.rewind(angpos[0])
 		# Get the polarization orientation on the sky
 		gamma = np.arctan2(xvec[2], -zvec[1]*xvec[0]+zvec[0]*xvec[1])
-		# Apply our extra polarization rotation
-		gamma += angpos[0]
+		# Apply our extra polarization rotation. See polrot_field.
+		gamma -= angpos[0]
 		return bunch.Bunch(angpos=angpos, gamma=gamma, delay=delay, pos=zvec)
 
 def calc_pixels(angpos, delay, wcs_pos, wcs_delay):

tod2ring.py
@@ -55,9 +55,9 @@ for fname in args.tods[comm.rank::comm.size]:
 	d   = d.reshape(d.shape[0], ntheta, nspin, ndelay)
 	# Undo the effect of drift in theta and spin during each stroke
 	d   = pixie.fix_drift(d)
-	# Fourier-decompose the spin
-	fd  = fft.rfft(d, axes=[2])
-	d   = np.array([fd[:,:,0].real, fd[:,:,2].real, -fd[:,:,2].imag])
+	# Fourier-decompose the spin. *2 for pol because <sin^2> = 0.5.
+	fd  = fft.rfft(d, axes=[2])/d.shape[2]
+	d   = np.array([fd[:,:,0].real, fd[:,:,2].real*2, -fd[:,:,2].imag*2])
 	# Go from stroke to spectrum
 	d   = fft.rfft(d).real[...,::2]*2/ndelay
 	# Reorder from [stokes,det,theta,phi,freq] to [det,freq,stokes,theta,phi]