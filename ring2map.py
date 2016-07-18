Fixed gain and rotation issue

ring2map.py
@@ -14,12 +14,13 @@ hdu   = fitsio.open(args.rings[0])[0]
 wcs   = enlib.wcs.WCS(hdu.header)
 shape = hdu.data.shape
 dtype = np.float32
+atpole= True
 
 ndet, nfreq, ncomp, ntheta, nphi = shape
 nphi    = int(np.ceil(360 / wcs.wcs.cdelt[0]))
 # Rings cover 360 degrees, but those are split into two
-# columns in our image.
-ntheta /= 2
+# columns in our image. The poles are part of both sides.
+ntheta = ntheta/2 + atpole
 
 # We want our output coordinate system to be centered on the equator
 # rather than beginning there. So offset crpix for theta
@@ -44,14 +45,24 @@ for ifile in args.rings[comm.rank::comm.size]:
 	iphi   = iphi   + np.arange(nring)*0
 	# Wrap onto sky
 	bad = itheta >= ntheta
-	itheta[bad] = 2*ntheta - itheta[bad] - 1
+	itheta[bad] = 2*ntheta - itheta[bad] - 1 - atpole
 	iphi  [bad] = iphi[bad] + nphi/2
 	bad = itheta < 0
-	itheta[bad] = -itheta[bad]
+	itheta[bad] = -itheta[bad] -1 + atpole
 	iphi  [bad] = iphi[bad] + nphi/2
 	iphi %= nphi
-	omap[:,:,itheta,iphi] = m[:,:,:,0]
+	#theta = theta + np.arange(nring)*360./nring
+	#for i in range(nring):
+	#	print "%4d %7.3f %4d %4d" % (i, theta[i], itheta[i], ntheta)
+	#1/0
+	omap[:,:,itheta,iphi] += m[:,:,:,0]
 	hits[itheta,iphi] += 1
+	# Copy over poles
+	if atpole:
+		for t in [0,ntheta-1]:
+			i = np.where(itheta == t)[0]
+			omap[:,:,itheta[i],(iphi[i]+nphi/2)%nphi] += m[:,:,i,0]
+			hits[itheta[i],(iphi[i]+nphi/2)%nphi] += 1
 
 omap = utils.allreduce(omap, comm)
 hits = utils.allreduce(hits, comm)

sim_freqmap.py
@@ -37,7 +37,7 @@ if args.apply_beam:
 	# Smooth manually using full-sky geometry
 	for field in fields:
 		print field.name
-		minfo = sharp.map_info_fejer1(field.map.shape[-2], field.map.shape[-1])
+		minfo = sharp.map_info_clenshaw_curtis(field.map.shape[-2], field.map.shape[-1])
 		ainfo = sharp.alm_info(lmax=beam_lmax)
 		sht   = sharp.sht(minfo, ainfo)
 		alm   = np.zeros((3,ainfo.nelem),dtype=complex)
@@ -62,6 +62,10 @@ if args.apply_beam:
 	maps = [map*scatter[:,None,None,None] for map in maps]
 maps.insert(0, np.sum(maps,0))
 maps = enmap.samewcs(np.array(maps), maps[1])
+
+# At this point we have a factor 4 higher values than our full
+# tod simulation. There is also a pixel shift issue, but that
+# should be simple.
 
 if args.unit:
 	if args.unit == "cmb":

tod2ring.py
@@ -58,18 +58,18 @@ for fname in args.tods[comm.rank::comm.size]:
 	orient= pgen.calc_orientation(elem)
 	point = pgen.calc_pointing(orient, elem.delay, np.array([0,0,0]))
 	phi   = point.angpos[0,0]
-	gamma0= point.gamma[0]
 	wcs.wcs.crpix[0] = 1 - phi/utils.degree/wcs.wcs.cdelt[0]
+	# Detector measures 1/4 of the signal due to how the interferrometry
+	# is set up.
+	gain  = 1.0/4
 	# Ger out tod samples
-	d   = tod.signal
+	d   = tod.signal / gain
 	d   = d.reshape(d.shape[0], ntheta, nspin, ndelay)
 	# Undo the effect of drift in theta and spin during each stroke
 	d   = pixie.fix_drift(d)
 	# Fourier-decompose the spin. *2 for pol because <sin^2> = 0.5.
 	fd  = fft.rfft(d, axes=[2])/d.shape[2]
 	d   = np.array([fd[:,:,0].real, fd[:,:,2].real*2, -fd[:,:,2].imag*2])
-	## Rotate polarization. Why is this needed?  Why is it spin 4?
-	#d   = pixie.rot_comps(d, -gamma0*4, 0)
 	# Go from stroke to spectrum. This takes us to W/sr/m^2/Hz
 	d   = fft.rfft(d).real[...,::2]*2/ndelay/dfreq
 	# Reorder from [stokes,det,theta,phi,freq] to [det,freq,stokes,theta,phi]